955f7f000d797ff253d9f484cc9180b0c01ebc8d
Daniela Ramon Feria <l19350318@tuxtepec.tecnm.mx>
2023-03-01 09:25:09 -0600

    Lista de Hobbies

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+//import { useState } from 'react'
+//import reactLogo from './assets/react.svg'
+//import './App.css'
+
+function App() {
+  const nombre = "Daniela Ramón Feria";
+  return (
+<div>
+  <h3>Lista de Hobbies de {nombre}</h3>
+  <ol>
+    <li>Jugar Videojuegos</li>
+    <li>Leer</li>
+    <li>Tocar Guitarra</li>
+    <li>Cantar</li>
+    <li>Bailar</li>
+  </ol>
+</div>
+  )
+}
+
+export default App
